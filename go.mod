b17ecd14544655cd263496dd7a55806325ae458e
isaacmcpherson <104937620+isaacmcpherson@users.noreply.github.com>
2023-10-29 21:26:17 -0500

    remove test functions

---
+++ go.mod
@@ -0,0 +1,8 @@
+module proj2
+
+go 1.21.1
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/mattn/go-sqlite3 v1.14.17
+)
--- main_test.go
+++ main_test.go
@@ -9,33 +9,6 @@ import (
 	"io"
 )
 
-/*func TestAuthHandler(t *testing.T) {
-	// Create a test server
-	server := httptest.NewServer(http.HandlerFunc(AuthHandler))
-	defer server.Close()
-
-	// Test with valid private key
-	resp, err := http.Get(server.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
-	}
-
-	// Test with expired private key
-	expiredURL := server.URL + "?expired=true"
-	resp, err = http.Get(expiredURL)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
-	}
-}*/
-
 func TestAuthHandler(t *testing.T) {
 	// Create a test server
 	server := httptest.NewServer(http.HandlerFunc(AuthHandler))
@@ -87,24 +60,4 @@ func TestJWKSHandler(t *testing.T) {
 	if !strings.Contains(string(body), expected) {
 		t.Errorf("Response body does not contain the expected content.")
 	}
-}
-
-func TestRetrieveValidKeys(t *testing.T) {
-	// Add test cases for retrieving valid keys from the database
-}
-
-func TestDecodePrivateKeyFromPEM(t *testing.T) {
-	// Add test cases for decoding private key from PEM
-}
-
-func TestMain(t *testing.T) {
-	// Add test cases for the main function (e.g., database setup)
-}
-
-func TestStoreKey(t *testing.T) {
-	// Add test cases for storing keys in the database
-}
-
-func TestEncodePrivateKeyToPEM(t *testing.T) {
-	// Add test cases for encoding private key to PEM
 }
